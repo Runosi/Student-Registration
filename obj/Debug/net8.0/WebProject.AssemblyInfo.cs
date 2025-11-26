06e78cb4a6ee0ebb167229c349cb4542b82bf594
Runosi <nwukemagdala@gmail.com>
2025-11-25 23:19:20 -0800

    another commit

--- obj/Debug/net8.0/WebProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb193bc47401f91adcadf468e3579b3e591498d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f2e34362d4b6342d5a87828d87b540867835d79")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
